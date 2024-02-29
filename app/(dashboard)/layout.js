18f4b322556cb5bf810baa3c1a7659c64c06bf45
Shamil Aliyev <samil.alieff@gmail.com>
2024-02-29 08:47:36 +0100

    Displaying current user's email

--- app/(dashboard)/layout.js
+++ app/(dashboard)/layout.js
@@ -4,14 +4,20 @@ import Navbar from "@/components/Navbar";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
-export default function layout({children}) {
+export default async function layout({children}) {
   const supabase = createServerComponentClient({cookies});
-  const {data} = supabase.auth.getSession()
+  const {data,error} = await supabase.auth.getSession()
   
+  if(error){
+    console.log(error)
+  }
+  if(!data.session){
+    redirect('/login')
+  }
   
   return (
     <>
-    <Navbar user={data}/>
+    <Navbar user={data.session.user}/>
         {children}
     <Footer />
     </>
